e71a0fe96da468f3ed4888936c84dd393ec6eea3
louvastola <vastlegacy@gmail.com>
2016-09-06 23:09:19 -0400

    First commit

---
+++ Bartender/Controllers/DrinksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bartender.Models;
+
+namespace Bartender.Controllers
+{
+    public class DrinksController : Controller
+    {
+        // GET: Drinks
+        public ActionResult Random()
+        {
+            var drink = new Drink() {Name = "Manhatten"};
+
+            return View(drink);
+        }
+    }
+}
---
+++ Bartender/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bartender.MvcApplication" Language="C#" %>
